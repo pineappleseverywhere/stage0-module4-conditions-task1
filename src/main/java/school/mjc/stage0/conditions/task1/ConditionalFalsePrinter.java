1023c87ce080d4cfa992fdef8b69bf90c6b1a57c
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-10 02:55:31 -0400

    done

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (5 > 7 != false) {
+
+        } else{
+            System.out.println("false");
+        }
     }
 }
